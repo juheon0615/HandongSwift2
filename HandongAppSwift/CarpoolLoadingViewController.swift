dd075fdc1a48e0b947e3bc0071c098e0dff1c500
ghost <juheon0615@gmail.com>
2015-07-30 16:47:20 +0900

    layout for carpool// ready to fork

---
+++ HandongAppSwift/CarpoolLoadingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CarpoolLoadingViewController.swift
+//  HandongAppSwift
+//
+//  Created by ghost on 2015. 7. 29..
+//  Copyright (c) 2015년 GHOST. All rights reserved.
+//
+
+import Foundation
---
+++ HandongAppSwift/NoticeLoading.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeLoading.swift
+//  HandongAppSwift
+//
+//  Created by ghost on 2015. 7. 28..
+//  Copyright (c) 2015년 GHOST. All rights reserved.
+//
+
+import Foundation
